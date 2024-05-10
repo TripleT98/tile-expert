84166a9529cef253a82962b0c15bf71765efe5d4
@ttulegenov <ttulegenov@rit-it.com>
2024-05-11 01:32:52 +0500

    add header

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { NavBarItem } from '@shared/models/nav-bar-item.model';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
+
+  @ViewChild('titleCont') titleContainer!: ElementRef;
+
+  protected navBarItems: NavBarItem[] = [
+    {
+      name: 'Ссылки',
+      iconName: 'link'
+    },
+    {
+      name: 'Контакты',
+      iconName: 'contact'
+    },
+    {
+      name: 'Теги',
+      iconName: 'tag'
+    },
+    {
+      name: 'Избранное',
+      iconName: 'favourite'
+    },
+    {
+      name: 'Посещения',
+      iconName: 'visit'
+    },
+  ];
 
   constructor(
 
   ){
-    console.log('asdasdasdasd')
+  }
+
+  ngAfterViewInit(){
   }
 
 }
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@shared/modules/mat-module/mat-module/mat-module.module';
+import { SharedComponentsModule } from '@shared/modules/shared-components/shared-components.module';
+import { DirectivesModule } from '@shared/modules/directives-module/directives.module';
 import { MainComponent } from './main.component';
+
 
 @NgModule({
 	imports: [
-		MaterialModule
+		MaterialModule,
+    SharedComponentsModule,
+    DirectivesModule,
 	],
 	declarations: [
 		MainComponent
